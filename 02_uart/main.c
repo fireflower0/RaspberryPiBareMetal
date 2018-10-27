c4994d1245ce29cecbbb53b0c7c7f262606eace0
fireflower0 <fireflower0@hotmail.co.jp>
2018-10-28 00:11:45 +0900

    Add a put_hex function in main.c of 02_uart

--- 02_uart/main.c
+++ 02_uart/main.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #define UARTDR      (*(volatile unsigned int *)0x09000000)
 #define UARTFR      (*(volatile unsigned int *)0x09000018)
 #define UARTFR_TXFF (1U << 5)
@@ -12,8 +14,34 @@ void put_str(char *str){
         put_char(*str++);
 }
 
+void put_hex(uint64_t num){
+    int      n    = 0;
+    uint64_t base = 16;
+    uint64_t d    = 1;
+    char buf[32], *bf;
+
+    bf = buf;
+    *bf++ = '0';
+    *bf++ = 'x';
+
+    while(d != 0){
+        int dgt = num / d;
+        num %= d;
+        d /= base;
+        if(n || dgt > 0 || d == 0){
+            *bf++ = dgt + (dgt < 10 ? '0' : ('A') - 10);
+            ++n;
+        }
+    }
+    *bf++ = '\n';
+    *bf = 0;
+
+    put_str(buf);
+}
+
 int main(void){
-    put_str("Hello, world!");
+    put_str("Hello, world!\n");
+    put_hex(10);
 
     while(1)
         ;
