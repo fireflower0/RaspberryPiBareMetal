ce2b0134c375f73c17abcda9c7d23a8c8a9153c8
fireflower0 <fireflower0@hotmail.co.jp>
2018-10-28 15:53:32 +0900

    Fix a main.c of 01_blink

--- 01_blink/main.c
+++ 01_blink/main.c
@@ -1,17 +1,33 @@
+#include <stdint.h>
+
 #define GPFSEL1 0x3F200004
 #define GPSET0  0x3F20001C
 #define GPCLR0  0x3F200028
 #define MAX_CNT 3000000
 
+///
+// Memory Mapped I/O
+
+void mmio_write(intptr_t reg, uint32_t data){
+    *(volatile uint32_t *)reg = data;
+}
+
+uint32_t mmio_read(intptr_t reg){
+    return *(volatile uint32_t *)reg;
+}
+
+///
+// Main Function
+
 int main(void){
     volatile unsigned int i;
 
-    *(volatile unsigned int *)GPFSEL1 = 0x01 << (3 * 6);
+    mmio_write(GPFSEL1, 0x01 << (3 * 6));
 
     while(1){
-        *(volatile unsigned int *)GPSET0 = 0x01 << 16;
+        mmio_write(GPSET0, 0x01 << 16);
         for(i = 0; i < MAX_CNT; i++);
-        *(volatile unsigned int *)GPCLR0 = 0x01 << 16;
+        mmio_write(GPCLR0, 0x01 << 16);
         for(i = 0; i < MAX_CNT; i++);
     }
 
