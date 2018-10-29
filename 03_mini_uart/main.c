09b2da0afcb4e797c6496d2d262b55c03dc32c57
fireflower0 <fireflower0@hotmail.co.jp>
2018-10-30 07:25:20 +0900

    Fix a main.c of 03_mini_uart

--- 03_mini_uart/main.c
+++ 03_mini_uart/main.c
@@ -2,8 +2,8 @@
 
 #define MU_IO           0x3F215040
 #define MU_LSR          0x3F215054
-#define MU_LSR_TX_IDLE	(1U << 6)
-#define MU_LSR_TX_EMPTY	(1U << 5)
+#define MU_LSR_TX_IDLE  (1U << 6)
+#define MU_LSR_TX_EMPTY (1U << 5)
 
 ///
 // Memory Mapped I/O
@@ -20,12 +20,11 @@ uint32_t mmio_read(intptr_t reg){
 // Main Function
 
 int main(void){
-	while (1) {
-		while (!(mmio_read(MU_LSR) & MU_LSR_TX_IDLE) &&
+    while (1) {
+        while (!(mmio_read(MU_LSR) & MU_LSR_TX_IDLE) &&
                !(mmio_read(MU_LSR) & MU_LSR_TX_EMPTY))
             ;
-		mmio_write(MU_IO, (uint32_t)'A');
-	}
-
-	return 0;
+        mmio_write(MU_IO, (uint32_t)'A');
+    }
+    return 0;
 }
