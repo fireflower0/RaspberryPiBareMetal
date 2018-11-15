2ababa2ff821d95b4c46cc624afb60f550fa598e
fireflower0 <fireflower0@hotmail.co.jp>
2018-11-16 07:05:19 +0900

    Fix 07_video3

--- 07_video3/main.c
+++ 07_video3/main.c
@@ -59,10 +59,20 @@ int mbox_call(unsigned char ch){
     return 0;
 }
 
-unsigned int width, height, pitch, bpp;
-unsigned char *lfb;
+typedef struct _fb_info_t {
+    unsigned int  display_w;  // display width
+    unsigned int  display_h;  // display height
+    unsigned int  w;          // framebuffer width
+    unsigned int  h;          // framebuffer height
+    unsigned int  row_bytes;  // write 0 to get value
+    unsigned int  bpp;        // bits per pixel
+    unsigned int  ofs_x;      // x offset of framebuffer
+    unsigned int  ofs_y;      // y offset of framebuffer
+    unsigned char *buf_addr;  // pointer to framebuffer
+    unsigned int  buf_size;   // framebuffer size in bytes
+} fb_info_t;
 
-void lfb_init(){
+void lfb_init(fb_info_t *fb_info){
     for(int i = 0; i < 36; i++){
         mbox[i] = 0;
     }
@@ -73,19 +83,19 @@ void lfb_init(){
     mbox[2] = 0x48003;  //set phy wh
     mbox[3] = 8;
     mbox[4] = 0;
-    mbox[5] = 1280;     //FrameBufferInfo.width
-    mbox[6] = 800;     //FrameBufferInfo.height
+    mbox[5] = fb_info->display_w;  //FrameBufferInfo.width
+    mbox[6] = fb_info->display_h;  //FrameBufferInfo.height
 
     mbox[7] = 0x48004;  //set virt wh
     mbox[8] = 8;
     mbox[9] = 0;
-    mbox[10] = 1280;     //FrameBufferInfo.virtual_width
-    mbox[11] = 800;     //FrameBufferInfo.virtual_height
+    mbox[10] = fb_info->w;        //FrameBufferInfo.virtual_width
+    mbox[11] = fb_info->h;        //FrameBufferInfo.virtual_height
     
     mbox[12] = 0x48005; //set depth
     mbox[13] = 4;
     mbox[14] = 0;
-    mbox[15] = 16;      //FrameBufferInfo.depth
+    mbox[15] = fb_info->bpp;      //FrameBufferInfo.depth
 
     mbox[16] = 0x40008; //get pitch
     mbox[17] = 4;
@@ -95,32 +105,40 @@ void lfb_init(){
     mbox[20] = 0x40001; //get framebuffer, gets alignment on request
     mbox[21] = 8;
     mbox[22] = 0;
-    mbox[23] = 16;      //FrameBufferInfo.pointer
+    mbox[23] = 0;       //FrameBufferInfo.pointer
     mbox[24] = 0;       //FrameBufferInfo.size
 
     mbox[25] = MBOX_TAG_LAST;
 
     if(mbox_call(MBOX_CH_PROP) && mbox[15] == 16 && mbox[23] != 0) {
         mbox[23] &= 0x3FFFFFFF;
-        width  = mbox[5];
-        height = mbox[6];
-        bpp    = mbox[15];
-        pitch  = mbox[19];
-        lfb = (void*)((unsigned long)mbox[23]);
+        fb_info->display_w = mbox[5];
+        fb_info->display_h = mbox[6];
+        fb_info->bpp       = mbox[15];
+        fb_info->row_bytes = mbox[19];
+        fb_info->buf_addr  = (void*)((unsigned long)mbox[23]);
     }
 }
 
-static inline void *coord2ptr(int x, int y) {
-    return (void *) (lfb + ((bpp + 7) >> 3) * x + pitch * y);
+static inline void *coord2ptr(unsigned char *vram,
+                              unsigned int pitch,
+                              unsigned int bpp,
+                              int x,
+                              int y) {
+    return (void *)(vram + ((bpp + 7) >> 3) * x + pitch * y);
 }
 
-void boxfill8(unsigned int c, int x0, int y0, int x1, int y1){
+void boxfill8(unsigned char *vram,
+              unsigned int c,
+              unsigned int pitch,
+              unsigned int bpp,
+              int x0, int y0, int x1, int y1){
     int x, y;
     unsigned short int *p;
 
     for (y = y0; y <= y1; y++) {
         for (x = x0; x <= x1; x++){
-            p = (unsigned short int *) coord2ptr(x, y);
+            p = (unsigned short int *)coord2ptr(vram, pitch, bpp, x, y);
             *p = c;
         }
     }
@@ -133,27 +151,32 @@ void boxfill8(unsigned int c, int x0, int y0, int x1, int y1){
 #define COL8_FFFFFF (((0xFF>>3)<<11) + ((0xFF>>3)<<6) + (0xFF>>3))
 
 int main(void){
-    lfb_init();
+    fb_info_t fb_info = {1280, 800, 1280, 800, 0, 16, 0, 0, 0, 0};
 
-    int x = width;
-    int y = height;
+    lfb_init(&fb_info);
 
-    boxfill8(COL8_008484,  0,     0,      x -  1, y - 29);
-    boxfill8(COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
-    boxfill8(COL8_FFFFFF,  0,     y - 27, x -  1, y - 27);
-    boxfill8(COL8_C6C6C6,  0,     y - 26, x -  1, y -  1);
+    unsigned char *vram  = fb_info.buf_addr;
+    unsigned int  pitch  = fb_info.row_bytes;
+    unsigned int  bpp    = fb_info.bpp;
+    unsigned int  x = fb_info.display_w;
+    unsigned int  y = fb_info.display_h;
 
-    boxfill8(COL8_FFFFFF,  3,     y - 24, 59,     y - 24);
-    boxfill8(COL8_FFFFFF,  2,     y - 24,  2,     y -  4);
-    boxfill8(COL8_848484,  3,     y -  4, 59,     y -  4);
-    boxfill8(COL8_848484, 59,     y - 23, 59,     y -  5);
-    boxfill8(COL8_000000,  2,     y -  3, 59,     y -  3);
-    boxfill8(COL8_000000, 60,     y - 24, 60,     y -  3);
+    boxfill8(vram, COL8_008484, pitch, bpp,  0,     0,      x -  1, y - 29);
+    boxfill8(vram, COL8_C6C6C6, pitch, bpp,  0,     y - 28, x -  1, y - 28);
+    boxfill8(vram, COL8_FFFFFF, pitch, bpp,  0,     y - 27, x -  1, y - 27);
+    boxfill8(vram, COL8_C6C6C6, pitch, bpp,  0,     y - 26, x -  1, y -  1);
 
-    boxfill8(COL8_848484, x - 47, y - 24, x -  4, y - 24);
-    boxfill8(COL8_848484, x - 47, y - 23, x - 47, y -  4);
-    boxfill8(COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
-    boxfill8(COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
+    boxfill8(vram, COL8_FFFFFF, pitch, bpp,  3,     y - 24, 59,     y - 24);
+    boxfill8(vram, COL8_FFFFFF, pitch, bpp,  2,     y - 24,  2,     y -  4);
+    boxfill8(vram, COL8_848484, pitch, bpp,  3,     y -  4, 59,     y -  4);
+    boxfill8(vram, COL8_848484, pitch, bpp, 59,     y - 23, 59,     y -  5);
+    boxfill8(vram, COL8_000000, pitch, bpp,  2,     y -  3, 59,     y -  3);
+    boxfill8(vram, COL8_000000, pitch, bpp, 60,     y - 24, 60,     y -  3);
+
+    boxfill8(vram, COL8_848484, pitch, bpp, x - 47, y - 24, x -  4, y - 24);
+    boxfill8(vram, COL8_848484, pitch, bpp, x - 47, y - 23, x - 47, y -  4);
+    boxfill8(vram, COL8_FFFFFF, pitch, bpp, x - 47, y -  3, x -  4, y -  3);
+    boxfill8(vram, COL8_FFFFFF, pitch, bpp, x -  3, y - 24, x -  3, y -  3);
 
     while(1)
         ;
