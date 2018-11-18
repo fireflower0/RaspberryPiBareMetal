be45695e6b5ba479a354a9e7481e139b35cfa5d3
fireflower0 <fireflower0@hotmail.co.jp>
2018-11-18 14:20:17 +0900

    Fix 03_mini_uart

--- 03_mini_uart/main.c
+++ 03_mini_uart/main.c
@@ -1,30 +1,24 @@
-#include <stdint.h>
+// RaspberryPi3 Memory Mapped I/O Base Address
+#define MMIO_BASE 0x3F000000
 
-#define MU_IO           0x3F215040
-#define MU_LSR          0x3F215054
+// Memory Mapped I/O
+#define IOREG(X)  (*(volatile unsigned int *) (X))
+
+// UART
+#define MU_IO           IOREG(MMIO_BASE + 0x00215040)
+#define MU_LSR          IOREG(MMIO_BASE + 0x00215054)
 #define MU_LSR_TX_IDLE  (1U << 6)
 #define MU_LSR_TX_EMPTY (1U << 5)
-
-///
-// Memory Mapped I/O
-
-void mmio_write(intptr_t reg, uint32_t data){
-    *(volatile uint32_t *)reg = data;
-}
-
-uint32_t mmio_read(intptr_t reg){
-    return *(volatile uint32_t *)reg;
-}
 
 ///
 // Main Function
 
 int main(void){
     while (1) {
-        while (!(mmio_read(MU_LSR) & MU_LSR_TX_IDLE) &&
-               !(mmio_read(MU_LSR) & MU_LSR_TX_EMPTY))
-            ;
-        mmio_write(MU_IO, (uint32_t)'A');
+        while (!(MU_LSR & MU_LSR_TX_IDLE) && !(MU_LSR & MU_LSR_TX_EMPTY)){
+            asm volatile("nop");
+        }
+        MU_IO = (unsigned int)'A';
     }
     return 0;
 }
